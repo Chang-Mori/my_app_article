40320bab035948e766ef68c53fe67382852aaab1
Chang-Mori <n0musi9.n0rice@gmail.com>
2021-01-25 01:11:02 +0900

    TOPへ戻るボタンをスクロール後フェードインする様に実装

--- app/javascript/packs/scrollTop.js
+++ app/javascript/packs/scrollTop.js
@@ -1,5 +1,21 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const PageTopBtn = document.getElementById('scroll-top');
+//   PageTopBtn.addEventListener('click', () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: 'smooth'
+//     });
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   const PageTopBtn = document.getElementById('scroll-top');
+  function getScrolled() {
+    return ( window.pageYOffset !== undefined ) ? window.pageYOffset: document.documentElement.scrollTop;
+  }
+  window.onscroll = () => {
+    ( getScrolled() > 1000 ) ? PageTopBtn.classList.add( 'fade-in' ): PageTopBtn.classList.remove( 'fade-in' );
+  };
   PageTopBtn.addEventListener('click', () => {
     window.scrollTo({
       top: 0,
